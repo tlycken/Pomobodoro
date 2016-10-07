f9cfc02a31b89e6c961e2293efb115a4922361e7
tlycken <tomas.lycken@gmail.com>
2016-10-07 21:02:25 +0200

    Setup of tooling
    Cory House at Pluralsight
    said to do like this

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream'); 
+var concat = require('gulp-concat');
+var eslint = require('gulp-eslint');
+
+var config = {
+    port: 4711,
+    baseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+        ],
+        mainJs: './src/main.js',
+        dist: './dist'
+    }
+};
+
+gulp.task('connect', function () {
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.baseUrl,
+        livereload: true
+    });
+});
+
+gulp.task('open', ['connect'], function() {
+    gulp.src('dist/index.html').pipe(open({
+        uri: config.baseUrl + ':' + config.port + '/'
+    }));
+});
+
+gulp.task('html', function() {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
+gulp.task('css', function() {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/styles'));
+});
+
+gulp.task('lint', function() {
+    return gulp.src(config.paths.js)
+        .pipe(eslint('eslint.config.json'))
+        .pipe(eslint.format());
+});
+
+gulp.task('watch', function() {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
+    gulp.watch(config.paths.css, ['css']);
+    gulp.watch('eslint.config.json', ['lint']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
